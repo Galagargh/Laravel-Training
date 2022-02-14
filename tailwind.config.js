9d6212ec8a04830a2a6074012322afda6b465c85
Matt Gallagher <matt@bigreach.co.uk>
2022-02-14 12:13:54 +0000

    Multiple Changes
    - changed "post" database to "project"
    - added TailwindCSS dependency
    - configured Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+      "resources/views/layout/layout.blade.php",
+      "resources/views/layout/project.blade.php",
+      "resources/views/layout/projects.blade.php",
+      "resources/views/layout/project.blade.php",
+      "resources/views/layout/projects.blade.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
