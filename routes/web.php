b15a98b0b8a2541b2bc870d512752e8ab7f70f6c
Galagargh <48443065+Galagargh@users.noreply.github.com>
2022-03-16 11:52:59 +0000

    Newsletter Functionality
    - Newsletter functionality implemented
    - Newsletter Interface added
    - Mailchimp API added as dependency
    - Mailchimp Service Added
    - Newsletter URI added in routes file

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 //if not signed in
 Route::middleware('guest')->group(function () {
